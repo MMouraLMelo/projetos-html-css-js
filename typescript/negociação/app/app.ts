779317ed64be72f3a0e1225c615f03e61a29b958
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-10-24 16:26:05 -0300

    atualizando

--- typescript/negociação/app/app.ts
+++ typescript/negociação/app/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
@@ -6,3 +7,5 @@ form.addEventListener('submit', event =>{
     event.preventDefault();
     controller.adiciona();
 });
+
+
