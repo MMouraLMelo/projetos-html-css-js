31ca04aec0c63e7073ac1a7e37b1335a6498e21b
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-10-25 10:26:34 -0300

    atualizando

--- typescript/negociação/app/views/negociacoes-view.ts
+++ typescript/negociação/app/views/negociacoes-view.ts
@@ -1,10 +1,8 @@
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView{
-    private elemento: HTMLElement;
-    constructor(seletor: string){
-        this.elemento = document.querySelector(seletor);
-    }
+export class NegociacoesView extends View<Negociacoes>{
+    
     template(model: Negociacoes): string{
         return `
         <table class="table table-hover table-bordered">
@@ -29,10 +27,6 @@ export class NegociacoesView{
         </table>
         `;
     }
-    update(model: Negociacoes): void{
-        const template = this.template(model);
-        console.log(template)
-        this.elemento.innerHTML = template;
-    }
+    
 }
 
