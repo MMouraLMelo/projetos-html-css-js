7aca370c5eb6683df123f302db7b6ff06142b0ea
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-10-26 15:23:54 -0300

    refreshing

--- typescript/negociação/app/app.ts
+++ typescript/negociação/app/app.ts
@@ -3,9 +3,13 @@ import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event =>{
-    event.preventDefault();
-    controller.adiciona();
+if (form){
+    form.addEventListener('submit', event =>{
+        event.preventDefault();
+        controller.adiciona();
 });
+}else{
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.')
+}
 
 
